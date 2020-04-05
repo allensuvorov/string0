154c0e3a9160aa5c877beb2dd808143df2827bdb
Allen <allenvs@mail.ru>
2020-04-05 22:46:19 +0300

    added solution code using slice and concat methods and lengh property

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-let str1 = "apples", str2 = "cucumber"
+let str1 = "apples", str2 = "cucumber";
+let str3 = str1.slice(0,str1.length/2).concat(str2.slice(str2.length/2,str2.length));
+console.log(str3);
